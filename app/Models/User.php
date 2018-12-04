4c4c8b0e4c57123c289f11001c81d625460c4326
Loedan <loedan.zhou@hwua.com>
2018-12-04 12:24:41 +0800

    Add user following

--- app/Models/User.php
+++ app/Models/User.php
@@ -59,7 +59,46 @@ class User extends Authenticatable
     # 用户发布的所有微博
     public function feed()
     {
-        return $this->statuses()
+        $user_ids = Auth::user()->followings->pluck('id')->toArray();
+        array_push($user_ids, Auth::user()->id);
+        return Status::whereIn('user_id', $user_ids)
+            ->with('user')
             ->orderBy('created_at', 'desc');
+    }
+
+    # 粉丝关系列表
+    public function followers()
+    {
+        return $this->belongsToMany(User::Class, 'followers', 'user_id', 'follower_id');
+    }
+
+    # 用户关注人列表
+    public function followings()
+    {
+        return $this->belongsToMany(User::Class, 'followers', 'follower_id', 'user_id');
+    }
+
+    # 进行关注
+    public function follow($user_ids)
+    {
+        if (!is_array($user_ids)) {
+            $user_ids = compact('user_ids');
+        }
+        $this->followings()->sync($user_ids, false);
+    }
+
+    # 取消关注
+    public function unfollow($user_ids)
+    {
+        if (!is_array($user_ids)) {
+            $user_ids = compact('user_ids');
+        }
+        $this->followings()->detach($user_ids);
+    }
+
+    # 判断用户是否关注了某个用户
+    public function isFollowing($user_id)
+    {
+        return $this->followings->contains($user_id);
     }
 }
